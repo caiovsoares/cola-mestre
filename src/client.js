c5c1d0a54c68df0b65cf0509472ff01b5270ae2a
caiovsoares <caio.vasoares@gmail.com>
2022-03-31 15:26:07 -0300

    funcionando

--- src/client.js
+++ src/client.js
@@ -1,16 +1,20 @@
 import { io } from 'socket.io-client'
 import {GlobalKeyboardListener} from "node-global-key-listener"
+import clipboard from 'clipboardy'
+const copy = clipboard.writeSync;
+const paste = clipboard.readSync;
 
 const socket = io("ws://localhost:3000");
 const listener = new GlobalKeyboardListener();
 
-listener.addListener(function (e, down) {
-    if(e.state == "DOWN")
-    console.log(e.name)
-    // if(e.name === "SPACE")
-    //     socket.emit("ping","Mensagem Personalizada", (resposta) => {
-    //         console.log(resposta)
-    //     })
+const keys = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
+listener.addListener(function (e, down) {
+
+    if(down['LEFT CTRL'] && down['LEFT SHIFT'] && e.state == "DOWN" && keys.includes(e.name))
+        socket.emit('SALVAR',e.name+paste()); 
+
+    if(down['LEFT CTRL'] && down['LEFT ALT'] && e.state == "DOWN" && keys.includes(e.name))
+        socket.emit('RECEBER',e.name, resposta => copy(resposta));
 
 });
--- src/server.js
+++ src/server.js
@@ -1,17 +1,33 @@
 import { Server } from 'socket.io';
 import clipboard from 'clipboardy';
+import {GlobalKeyboardListener} from "node-global-key-listener"
 
 const copy = clipboard.writeSync;
-
+const paste = clipboard.readSync;
 const io = new Server({ /* options */ });
+const listener = new GlobalKeyboardListener();
+const keys = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const database = {}
+const save = (str) => database[str.substring(0,1)] = str.substring(1) 
+const retrieve = (str) => database[str]
+
+listener.addListener(function (e, down) {
+
+    if(down['LEFT CTRL'] && down['LEFT SHIFT'] && e.state == "DOWN" && keys.includes(e.name))
+        save(e.name+paste())
+
+    if(down['LEFT CTRL'] && down['LEFT ALT'] && e.state == "DOWN" && keys.includes(e.name))
+        copy(retrieve(e.name))
+
+});
 
 io.on("connection", (socket) => {
 
-  socket.on("ping", (arg, callback) => {
-    copy(arg)
-    callback("reCba!")
-  })
-  
+    socket.on("SALVAR", arg => save(arg))
+
+    socket.on("RECEBER", (arg, callback) => {
+        callback(retrieve(arg))
+    })
 });
 
 io.listen(3000);
